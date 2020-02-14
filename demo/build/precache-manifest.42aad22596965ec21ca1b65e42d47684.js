009cc4852d0768a54ab74f29a77f409d5541e541
Naser Afsar <chita@Nasers-MacBook-Pro.local>
2020-02-13 21:48:41 -0700

    new

---
+++ demo/build/precache-manifest.42aad22596965ec21ca1b65e42d47684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e44b69e7e6b987d9eeca21850ed02b2",
+    "url": "/demo/react-app/index.html"
+  },
+  {
+    "revision": "e04349a1546111a1b86f",
+    "url": "/demo/react-app/static/css/main.dfe17f73.chunk.css"
+  },
+  {
+    "revision": "34b4af31582bb5da9044",
+    "url": "/demo/react-app/static/js/2.7cfc5464.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/demo/react-app/static/js/2.7cfc5464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04349a1546111a1b86f",
+    "url": "/demo/react-app/static/js/main.d8889e83.chunk.js"
+  },
+  {
+    "revision": "e35a914cb4c2fd438e01",
+    "url": "/demo/react-app/static/js/runtime-main.ce38ac12.js"
+  }
+]);
